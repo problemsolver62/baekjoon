f5451bd5d67b8bd45415f2f91800d4f87f641c08
ProblemSolver <137838397+problemsolver62@users.noreply.github.com>
2023-06-27 16:58:56 +0900

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  int t;
+  cin >> t;
+  while (t--) 
+  {
+    int count = 0;
+    int x1, y1, r1, x2, y2, r2;
+    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+    double dis = sqrt(pow(x1 - x2, 2) + pow((y1 - y2), 2));
+    int rsum = r1 + r2;
+    int rsub = (r1 > r2) ? r1 - r2 : r2 - r1;
+    if (dis != 0) 
+    {
+      if (rsum == dis)
+        count++;
+      else if (rsub == dis)
+        count++;
+      else if (rsum < dis)
+        count += 0;
+      else if (dis < rsub)
+        count += 0;
+      else
+        count += 2;
+    } else if (r1 == r2)
+      count = -1;
+    cout << count << '\n';
+  }
+}
